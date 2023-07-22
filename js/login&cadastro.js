92b51119a74187405ffcad9ccd6aee2ceca9cb5b
Daniel Souza <danieldesouza1024@gmail.com>
2023-07-21 22:39:59 -0300

    Resolvendo o Problema do Cadastro

--- js/login&cadastro.js
+++ js/login&cadastro.js
@@ -1,12 +1,15 @@
+// var cadastroEmail =  window.prompt("Informe seu gmail para cadastrar")
+// var cadastroPassword =  window.prompt("Informe sua senha para cadastrar")
+// window.confirm("Seu Email é senha são " + cadastroEmail + cadastroPassword)
+let btnCadastro = document.querySelector(".btn-Cadastro");
 let btnEntrar = document.querySelector(".btn-entrar");
+
 let btnEntendi = document.querySelector(".btn-entendi")
 let alerta = document.querySelector(".alerta")
 let conteinerAviso = document.querySelector(".conteiner-aviso")
 
 
-// Dados da Conta
-let password = document.getElementById("password").value;
-let email = document.getElementById("email").value;
+
 btnEntrar.addEventListener("click" , ()=>{
     let cadastro = document.querySelector(".cadastro")
     cadastro.classList.add("paraEsquerda")
@@ -18,11 +21,20 @@ btnEntendi.addEventListener("click" , ()=>{
     alerta.classList.remove("ativado")
     conteinerAviso.classList.remove("ativado")
 })
+
+// Dados da Conta
+let passwordCadastro = document.getElementById("password").value;
+let emailCadastro  = document.getElementById("email").value;
+// Dados  Login
+let passwordLogin = document.getElementById("passwordLogin").value;
+let emailLogin = document.getElementById("emailLogin").value;
 function VerificarDados(){
-    if(password == password && email == email){
+    if(emailCadastro == emailLogin && passwordCadastro == passwordLogin){
         window.alert("acertou mizeravi")
+        // console.log(emailCadastro + passwordCadastro + emailLogin + passwordLogin)
+        // location.href = "home.html"
     }
     else{
-        window.alert("Erouuu")
+        window.alert("Errou")
     }
 }
